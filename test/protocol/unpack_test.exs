150cbb49c6b6fe673b9cc43119687faf950d1698
av <av@fun-box.ru>
2015-09-13 21:50:47 +0300

    SMPPEX.Protocol.Unpack.integer tests

--- test/protocol/unpack_test.exs
+++ test/protocol/unpack_test.exs
@@ -1,8 +1,17 @@
 defmodule SMPPEX.Protocol.UnpackTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import SMPPEX.Protocol.Unpack
+
+  test "integer" do
+    assert match? {:error, _}, integer(<<>>, 1)
+    assert match? {:error, _}, integer(<<1>>, 2)
+    assert match? {:error, _}, integer(<<1,2,3>>, 4)
+    assert match? {:error, _}, integer(<<1,2,3>>, 3)
+
+    assert {:ok, 1, <<2,3>>} == integer(<<1,2,3>>, 1)
+    assert {:ok, 1, <<3>>} == integer(<<0,1,3>>, 2)
+    assert {:ok, 1, <<5>>} == integer(<<0,0,0,1,5>>, 4)
   end
 end
 
