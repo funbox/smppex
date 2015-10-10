a2246f09ff9aa32b54f44f7c9153b4c8629cb8e7
av <av@fun-box.ru>
2015-10-10 11:09:36 +0300

    pack Integer and C-Octet String tests

--- lib/smppex/protocol/pack.ex
+++ lib/smppex/protocol/pack.ex
@@ -17,7 +17,7 @@ defmodule SMPPEX.Protocol.Pack do
   @invalid_tlv_tag "TLV: invalid tag"
   @invalid_tlv_value "TLV: invalid value"
 
-  def integer(int) when not is_integer(int), do: error(@invalid_integer)
+  def integer(int, _size) when not is_integer(int), do: error(@invalid_integer)
 
   def integer(int, size) when (size == 1 or size == 2 or size == 4) do
     integer_bit_size = size * 8
@@ -28,24 +28,26 @@ defmodule SMPPEX.Protocol.Pack do
     end
   end
 
+  def c_octet_string(nil, length_spec), do: c_octet_string("", length_spec, :ascii)
   def c_octet_string(str, length_spec), do: c_octet_string(str, length_spec, :ascii)
 
   def c_octet_string(str, _spec, _kind) when not is_binary(str), do: error(@invalid_c_octet_string_value)
+  def c_octet_string(nil, length_spec, kind), do: c_octet_string("", length_spec, kind)
 
   def c_octet_string(str, {:fixed, length}, kind) when is_integer(length) and length >= 1 do
-    c_octet_string(str, &(&1 == length), kind)
+    c_octet_string_with_general_check(str, &(&1 == length), kind)
   end
 
   def c_octet_string(str, {:max, max}, kind) when is_integer(max) and max >= 1 do
-    c_octet_string(str, &(&1 <= max), kind)
+    c_octet_string_with_general_check(str, &(&1 <= max), kind)
   end
 
-  def c_octet_string(str, check, kind) when is_function(check) do
+  defp c_octet_string_with_general_check(str, check, kind) when is_function(check) do
     str_length = byte_size(str)
     cond do
       str == nil -> ok(@null_str)
       str == "" -> ok(@null_str)
-      check.(str_length) ->
+      check.(str_length + 1) ->
         if valid_kind?(str, kind) do
           ok( str <> @null_str )
         else
---
+++ test/protocol/pack_test.exs
@@ -0,0 +1,66 @@
+defmodule SMPPEX.Protocol.PackTest do
+  use ExUnit.Case
+
+  import SMPPEX.Protocol.Pack
+
+  test "integer" do
+    assert_raise FunctionClauseError, fn() -> integer(123, 3) end
+
+    assert {:error, _} = integer(:bad_int, 1)
+
+    assert {:error, _} = integer(256, 1)
+    assert {:error, _} = integer(65536, 1)
+    assert {:error, _} = integer(4294967296, 1)
+
+    assert {:error, _} = integer(-1, 1)
+    assert {:error, _} = integer(-1, 1)
+    assert {:error, _} = integer(-1, 1)
+
+    assert {:ok, <<0xFF>>} = integer(255, 1)
+    assert {:ok, <<0xFF, 0xFF>>} = integer(65535, 2)
+    assert {:ok, <<0xFF, 0xFF, 0xFF, 0xFF>>} = integer(4294967295, 4)
+
+    assert {:ok, <<0x00>>} = integer(0, 1)
+    assert {:ok, <<0x00, 0x00>>} = integer(0, 2)
+    assert {:ok, <<0x00, 0x00, 0x00, 0x00>>} = integer(0, 4)
+  end
+
+  test "c_octet_string" do
+    assert_raise FunctionClauseError, fn() -> c_octet_string("foo", {:bad, :spec}) end
+    assert_raise FunctionClauseError, fn() -> c_octet_string("foo", {:max, 123}, :bad_format) end
+    assert {:error, _} = c_octet_string(:not_a_string, {:max, 123})
+  end
+
+  test "c_octet_string, fixed" do
+    assert {:error, _} = c_octet_string("abc", {:fixed, 3})
+    assert {:error, _} = c_octet_string("a", {:fixed, 3})
+
+    assert {:error, _} = c_octet_string("aX", {:fixed, 3}, :hex)
+    assert {:error, _} = c_octet_string("0X", {:fixed, 3}, :dec)
+
+    assert {:ok, << 0 >>} = c_octet_string("", {:fixed, 3})
+    assert {:ok, << 0 >>} = c_octet_string(nil, {:fixed, 3})
+    assert {:ok, << ?a, ?b, 0 >>} = c_octet_string("ab", {:fixed, 3})
+
+    assert {:ok, <<?a, ?b, 0>>} = c_octet_string("ab", {:fixed, 3}, :hex)
+    assert {:ok, <<?0, ?1, 0>>} = c_octet_string("01", {:fixed, 3}, :dec)
+  end
+
+  test "c_octet_string, var" do
+    assert {:error, _} = c_octet_string("abc", {:max, 3})
+
+    assert {:error, _} = c_octet_string("aX", {:max, 3}, :hex)
+    assert {:error, _} = c_octet_string("0X", {:max, 3}, :dec)
+
+    assert {:ok, << 0 >>} = c_octet_string("", {:max, 3})
+    assert {:ok, << 0 >>} = c_octet_string(nil, {:max, 3})
+
+    assert {:ok, << ?a, 0 >>} = c_octet_string("a", {:max, 3})
+    assert {:ok, << ?a, ?b, 0 >>} = c_octet_string("ab", {:max, 3})
+
+    assert {:ok, <<?a, ?b, 0>>} = c_octet_string("ab", {:max, 3}, :hex)
+    assert {:ok, <<?0, ?1, 0>>} = c_octet_string("01", {:max, 3}, :dec)
+  end
+
+end
+
