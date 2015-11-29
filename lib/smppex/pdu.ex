582a3fb8cef4802ac8cbc3f2fbc4bc4885f372f7
av <av@fun-box.ru>
2015-11-29 20:42:21 +0300

    Pdu releated modules typespecs added

--- lib/smppex/pdu.ex
+++ lib/smppex/pdu.ex
@@ -1,13 +1,14 @@
 defmodule SMPPEX.Pdu do
   alias SMPPEX.Protocol.TlvFormat
+  alias SMPPEX.Protocol.CommandNames
   alias SMPPEX.Pdu
 
   @type t :: %Pdu{
     command_id: integer,
     command_status: integer,
     sequence_number: integer,
-    mandatory: map(),
-    optional: map()
+    mandatory: map,
+    optional: map
   }
 
   defstruct [
@@ -17,6 +18,10 @@ defmodule SMPPEX.Pdu do
     mandatory: %{},
     optional: %{},
   ]
+
+  @type header :: {non_neg_integer, non_neg_integer, non_neg_integer}
+
+  @spec new(header, map, map) :: t
 
   def new({command_id, command_status, sequence_number}, mandatory_fields, optional_fields) do
     %Pdu{
@@ -28,21 +33,38 @@ defmodule SMPPEX.Pdu do
     }
   end
 
+  @spec command_id(t) :: non_neg_integer
+
   def command_id(pdu) do
     pdu.command_id
   end
+
+  @spec command_status(t) :: non_neg_integer
 
   def command_status(pdu) do
     pdu.command_status
   end
 
+  @spec sequence_number(t) :: non_neg_integer
+
   def sequence_number(pdu) do
     pdu.sequence_number
   end
 
+  @spec mandatory_field(t, integer | atom) :: any
+
   def mandatory_field(pdu, name) when is_atom(name) do
     pdu.mandatory[name]
   end
+
+  def mandatory_field(pdu, id) when is_integer(id) do
+    case CommandNames.name_by_id(id) do
+      {:ok, name} -> pdu.mandatory[name]
+      :unknown -> nil
+    end
+  end
+
+  @spec optional_field(t, integer | atom) :: any
 
   def optional_field(pdu, id) when is_integer(id) do
     pdu.optional[id]
@@ -55,11 +77,17 @@ defmodule SMPPEX.Pdu do
     end
   end
 
+  @spec field(t, integer | atom) :: any
+
   def field(pdu, id_or_name) do
     mandatory_field(pdu, id_or_name) || optional_field(pdu, id_or_name)
   end
 
+  @spec optional_fields(t) :: map
+
   def optional_fields(pdu), do: pdu.optional
+
+  @spec mandatory_fields(t) :: map
 
   def mandatory_fields(pdu), do: pdu.mandatory
 
