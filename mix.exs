596f0b70820aa6ee760575f4d0052ff81f2186e0
av <av@fun-box.ru>
2016-01-18 09:53:26 +0300

    ranch hex added

--- mix.exs
+++ mix.exs
@@ -26,7 +26,8 @@ defmodule Smppex.Mixfile do
       {:dialyxir, git: "https://github.com/jeremyjh/dialyxir.git", only: :dev},
       {:credo, "~> 0.1.0", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.11", only: :dev}
+      {:ex_doc, "~> 0.11", only: :dev},
+      {:ranch, "~> 1.2"}
     ]
   end
 end
