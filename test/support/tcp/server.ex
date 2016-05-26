5639719924cad7d6a647f6454c637ca109a4d477
av <av@fun-box.ru>
2016-05-26 23:37:48 +0300

    Added simple tcp server

---
+++ test/support/tcp/server.ex
@@ -0,0 +1,69 @@
+defmodule Support.TCP.SimpleServer do
+  defstruct [
+    :server_pid,
+    :received_data_pid,
+    :port
+  ]
+
+  alias Support.TCP.SimpleServer
+
+  def start_link do
+    port = Support.TCP.Helpers.find_free_port
+
+    {:ok, received_data_pid} = Agent.start_link(fn() -> %{data: <<>>, messages: []} end)
+
+    server_pid = spawn_link(fn() -> listen(port, received_data_pid) end)
+
+    %SimpleServer{server_pid: server_pid, received_data_pid: received_data_pid, port: port}
+  end
+
+  def port(server), do: server.port
+
+  def stop(server) do
+    server.server_pid ! :tcp_close
+    Agent.stop(server.received_data_pid)
+  end
+
+  def send(server, data) do
+    server.server_pid ! {:tcp_send, data}
+  end
+
+  def messages(server) do
+    Agent.get(server.received_data_pid, fn(received_data) -> Enum.reverse(received_data.messages) end)
+  end
+
+  def received_data(server) do
+    Agent.get(server.received_data_pid, fn(received_data) -> received_data.data end)
+  end
+
+  defp listen(port, received_data_pid) do
+    {:ok, listen_sock} = :gen_tcp.listen(port, [:binary])
+    {:ok, sock} = :gen_tcp.accept(listen_sock, {})
+    :ok = :gen_tcp.close(listen_sock)
+    loop(received_data_pid, sock)
+  end
+
+  defp loop(received_data_pid, sock) do
+    receive do
+      {:tcp, ^sock, data} = message ->
+        Agent.update(received_data_pid, fn(received_data) -> %{received_data: received_data.data <> data, messages: [message | received_data.messages]} end)
+        loop(received_data_pid, sock)
+      {:tcp_closed, ^sock} = message ->
+        Agent.update(received_data_pid, fn(received_data) -> %{received_data | messages: [message | received_data.messages]} end)
+        :gen_tcp.close(sock)
+      {:tcp_error, ^sock, reason} = message ->
+        Agent.update(received_data_pid, fn(received_data) -> %{received_data | messages: [message | received_data.messages]} end)
+        :gen_tcp.close(sock)
+      {:tcp_send, data} ->
+        :gen_tcp.send(sock, data)
+        loop(received_data_pid, sock)
+      :tcp_close ->
+        :gen_tcp.close(sock)
+    after
+      1_000 ->
+        :gen_tcp.close(sock)
+    end
+  end
+
+end
+
