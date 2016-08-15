069a42964d869a6d0ef2aacd0b97fa5d570cdd70
av <av@fun-box.ru>
2016-08-16 00:14:24 +0300

    Added module for converting Pdus to and from oserl format

---
+++ lib/smppex/pdu/oserl.ex
@@ -0,0 +1,58 @@
+defmodule SMPPEX.Pdu.Oserl do
+
+  alias SMPPEX.Pdu
+  alias SMPPEX.Protocol.TlvFormat
+
+  def to(pdu) do
+    {
+      Pdu.command_id(pdu),
+      Pdu.command_status(pdu),
+      Pdu.sequence_number(pdu),
+      fields_to_list(pdu)
+    }
+  end
+
+  def from({command_id, command_status, sequence_number, field_list}) do
+    {mandatory, optional} = list_to_fields(field_list)
+    Pdu.new(
+      {command_id, command_status, sequence_number},
+      mandatory,
+      optional
+    )
+  end
+
+  defp fields_to_list(pdu) do
+    (pdu |> Pdu.mandatory_fields |> Map.to_list) ++
+      (pdu |> Pdu.optional_fields |> Map.to_list |> ids_to_names)
+  end
+
+  defp list_to_fields(list, mandatory \\ %{}, optional \\ %{})
+
+  defp list_to_fields([], mandatory, optional), do: {mandatory, optional}
+  defp list_to_fields([{name, value} | list], mandatory, optional) do
+    case kind(name) do
+      {:optional, id} -> list_to_fields(list, mandatory, Map.put(optional, id, value))
+      :mandatory -> list_to_fields(list, Map.put(mandatory, name, value), optional)
+    end
+  end
+
+  defp kind(id) when is_integer(id), do: {:optional, id}
+  defp kind(name) when is_atom(name) do
+    case TlvFormat.id_by_name(name) do
+      {:ok, id} -> {:optional, id}
+      :unknown -> :mandatory
+    end
+  end
+
+  defp ids_to_names(by_ids, by_names \\ [])
+
+  defp ids_to_names([], by_names), do: by_names
+  defp ids_to_names([{name, value} | by_ids], by_names) when is_atom(name), do: ids_to_names(by_ids, [{name, value} | by_names])
+  defp ids_to_names([{id, value} | by_ids], by_names) when is_integer(id) do
+    case TlvFormat.name_by_id(id) do
+      {:ok, name} -> ids_to_names(by_ids, [{name, value} | by_names])
+      :unknown -> ids_to_names(by_ids, by_names)
+    end
+  end
+
+end
---
+++ test/pdu/oserl_test.exs
@@ -0,0 +1,31 @@
+defmodule SMPPEX.Pdu.OserlTest do
+
+  use ExUnit.Case
+
+  alias SMPPEX.Pdu
+  alias SMPPEX.Pdu.Oserl
+
+  test "to" do
+    pdu = Pdu.new(
+      {1, 2, 3},
+      %{ short_message: "message" },
+      %{ 0x0424 => "message payload" }
+    )
+
+    assert {1, 2, 3, fields} = Oserl.to(pdu)
+    assert [{:message_payload, "message payload"}, {:short_message, "message"}] == Enum.sort(fields)
+  end
+
+  test "from" do
+    oserl_pdu = {1, 2, 3, [{:message_payload, "message payload"}, {:short_message, "message"}]}
+
+    pdu = Oserl.from(oserl_pdu)
+
+    assert 1 == Pdu.command_id(pdu)
+    assert 2 == Pdu.command_status(pdu)
+    assert 3 == Pdu.sequence_number(pdu)
+    assert "message" == Pdu.mandatory_field(pdu, :short_message)
+    assert "message payload" == Pdu.optional_field(pdu, :message_payload)
+
+  end
+end
