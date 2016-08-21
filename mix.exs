0fa35f05deff2cf4087d1ca8fff7ebc026b95a40
av <av@fun-box.ru>
2016-08-21 19:43:30 +0300

    Updated deps

--- mix.exs
+++ mix.exs
@@ -30,7 +30,6 @@ defmodule Smppex.Mixfile do
     [
       {:coverex, "~> 1.4.1", only: :test},
       {:dialyxir, git: "https://github.com/jeremyjh/dialyxir.git", only: :dev},
-      {:credo, "~> 0.1.0", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
       {:ranch, "~> 1.2"},
