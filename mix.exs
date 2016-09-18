d1ec27ef96595dbcdcc579eb34998bb3c39df5a8
av <av@fun-box.ru>
2016-09-18 17:21:11 +0300

    Compile support modules only in :test env

--- mix.exs
+++ mix.exs
@@ -4,13 +4,13 @@ defmodule Smppex.Mixfile do
   def project do
     [
       app: :smppex,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps,
       description: description,
-      elixirc_paths: ["lib", "test/support"],
+      elixirc_paths: elixirc_paths(Mix.env),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         "coveralls": :test,
@@ -46,6 +46,9 @@ defmodule Smppex.Mixfile do
   defp description do
     "SMPP 3.4 protocol and framework implemented in Elixir"
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp package do
     [
