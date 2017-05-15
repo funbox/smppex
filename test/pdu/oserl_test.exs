d08fcc2134e8197826aa8cb0ebf5c1637b272da2
o.kudelin <o.kudelin@fun-box.ru>
2017-05-15 12:15:06 +0400

    Convert binary to list for know field and keen unknow fields as is

--- test/pdu/oserl_test.exs
+++ test/pdu/oserl_test.exs
@@ -9,15 +9,15 @@ defmodule SMPPEX.Pdu.OserlTest do
     pdu = Pdu.new(
       {1, 2, 3},
       %{short_message: "message"},
-      %{0x0424 => "message payload"}
+      %{0x0424 => "message payload", 0x1424 => "any tlv field"}
     )
 
     assert {1, 2, 3, fields} = Oserl.to(pdu)
-    assert [{:message_payload, "message payload"}, {:short_message, "message"}] == Enum.sort(fields)
+    assert [{5156, "any tlv field"}, {:message_payload, 'message payload'}, {:short_message, 'message'}] == Enum.sort(fields)
   end
 
   test "from" do
-    oserl_pdu = {1, 2, 3, [{:message_payload, "message payload"}, {:short_message, "message"}]}
+    oserl_pdu = {1, 2, 3, [{:message_payload, 'message payload'}, {:short_message, 'message'}, {5156, "any tlv field"}]}
 
     pdu = Oserl.from(oserl_pdu)
 
@@ -26,6 +26,6 @@ defmodule SMPPEX.Pdu.OserlTest do
     assert 3 == Pdu.sequence_number(pdu)
     assert "message" == Pdu.mandatory_field(pdu, :short_message)
     assert "message payload" == Pdu.optional_field(pdu, :message_payload)
-
+    assert "any tlv field" == Pdu.optional_field(pdu, 0x1424)
   end
 end
