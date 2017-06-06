8ec9c2db2d24deb76d061dcec872e3f3d09cf776
av <av@fun-box.ru>
2017-06-06 22:23:25 +0300

    Version bumpup

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Smppex.Mixfile do
   def project do
     [
       app: :smppex,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
