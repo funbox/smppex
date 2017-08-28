d06271a0fb3687bb970cf01e893d3b262e907d14
av <av@fun-box.ru>
2017-08-28 19:53:26 +0300

    Added general MC docs

--- lib/smppex/mc.ex
+++ lib/smppex/mc.ex
@@ -1,11 +1,47 @@
 defmodule SMPPEX.MC do
+  @moduledoc """
+  This is a module for launching a TCP listener (or any other listener supported
+  by `ranch`, for example, `ssl`) which handles incoming connections with the
+  passed `SMPPEX.Session` implementations.
+
+  To start an MC one generally should do the following.
+
+  1. Implement an `SMPPEX.Session` behaviour.
+
+  ```elixir
+
+  defmodule MyMCSession do
+    use SMPPEX.Session
+
+    # ...Callback implementation
+
+  end
+
+  ```
+
+  2. Start a listener passing implemented behaviour as a callback module.
+
+  ```elixir
+
+  {:ok, listener} = SMPPEX.MC.start({MyESMESession, some_args},
+                                    transport_opts: [port: 2775])
+  ```
+
+  The important things to note are:
+  * There is no `start_link` method, since started listener is not a standalone
+  `GenServer` but a pool of socket acceptors running under `Ranch` supervisor.
+  * Each received connection is served with its own process which uses passed
+  callback module (`MyESMESession`) for handling connection events.
+  """
 
   alias :ranch, as: Ranch
 
   @default_transport :ranch_tcp
   @default_acceptor_count 50
 
-  @spec start({module, args :: term}, opts :: Keyword.t) :: {:ok, listener_ref :: Ranch.ref} | {:error, reason :: term}
+  @spec start({module, args :: term}, opts :: Keyword.t) ::
+    {:ok, listener_ref :: Ranch.ref} |
+    {:error, reason :: term}
 
   def start({_module, _args} = mod_with_args, opts \\ []) do
     acceptor_count = Keyword.get(opts, :acceptor_count, @default_acceptor_count)
