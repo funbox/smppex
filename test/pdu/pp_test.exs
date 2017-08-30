daef6df814414f5afab2cc7b719ac82d79fbd3fa
av <av@fun-box.ru>
2017-08-30 11:50:54 +0300

    Added some SMPPEX.PP cases

--- test/pdu/pp_test.exs
+++ test/pdu/pp_test.exs
@@ -8,9 +8,19 @@ defmodule SMPPEX.Pdu.PPTest do
 
   test "format" do
     pdu = Factory.submit_sm({"from", 1, 2}, {"to", 1, 2}, "message")
+
+    # Just check that the function does not fail and that its result is printable
     assert pdu |> PP.format |> Erlang.iolist_to_binary
   end
 
+  test "format: error status and tlvs" do
+    pdu = SMPPEX.Pdu.new(
+      {0x01, 0x01, 0x01},
+      %{},
+      %{0x0005 => 1, 0x2222 => "val"}
+    )
+
+    # Just check that the function does not fail and that its result is printable
+    assert pdu |> PP.format |> Erlang.iolist_to_binary
+  end
 end
-
-
