e7b7e4ead93d0147bfda62b75801bb86956b116e
av <av@fun-box.ru>
2017-09-11 01:00:32 +0300

    Updated docs

--- lib/smppex.ex
+++ lib/smppex.ex
@@ -199,8 +199,6 @@ defmodule SMPPEX do
           {:ok, [PduFactory.submit_sm_resp(0, to_string(last_id)) |> Pdu.as_reply_to(pdu)], last_id + 1}
         :bind_transmitter ->
           {:ok, [PduFactory.bind_transmitter_resp(0) |> Pdu.as_reply_to(pdu)], last_id}
-        :enquire_link ->
-          {:ok, [PduFactory.enquire_link_resp |> Pdu.as_reply_to(pdu)], last_id}
         _ ->
           {:ok, last_id}
       end
