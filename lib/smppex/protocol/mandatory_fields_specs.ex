eb7cd0bf44e8b3bfe838d08792128411ea04d7d8
Ilya Averyanov <av@fun-box.ru>
2017-10-27 22:14:50 +0300

    Got rid of macros in MandatoryFieldsSpecs module

--- lib/smppex/protocol/mandatory_fields_specs.ex
+++ lib/smppex/protocol/mandatory_fields_specs.ex
@@ -10,278 +10,229 @@ defmodule SMPPEX.Protocol.MandatoryFieldsSpecs do
 
   @spec spec_for(atom) :: fields_spec
 
-  def spec_for(:bind_transmitter) do
-    [
-      {:system_id, {:c_octet_string, {:max, 16}}},
-      {:password,  {:c_octet_string, {:max, 9}}},
-      {:system_type,  {:c_octet_string, {:max, 13}}},
-      {:interface_version, {:integer, 1}},
-      {:addr_ton, {:integer, 1}},
-      {:addr_npi, {:integer, 1}},
-      {:address_range, {:c_octet_string, {:max, 41}}}
-    ]
+  def spec_for(name) do
+    specs() |> Map.get(name, [])
   end
 
-  def spec_for(:bind_transmitter_resp) do
-    [
-      {:system_id, {:c_octet_string, {:max, 16}}}
-    ]
-  end
+  defp specs do
+    %{
+      bind_transmitter: [
+        {:system_id, {:c_octet_string, {:max, 16}}},
+        {:password,  {:c_octet_string, {:max, 9}}},
+        {:system_type,  {:c_octet_string, {:max, 13}}},
+        {:interface_version, {:integer, 1}},
+        {:addr_ton, {:integer, 1}},
+        {:addr_npi, {:integer, 1}},
+        {:address_range, {:c_octet_string, {:max, 41}}}
+      ],
 
-  def spec_for(:bind_receiver) do
-    [
-      {:system_id, {:c_octet_string, {:max, 16}}},
-      {:password,  {:c_octet_string, {:max, 9}}},
-      {:system_type,  {:c_octet_string, {:max, 13}}},
-      {:interface_version, {:integer, 1}},
-      {:addr_ton, {:integer, 1}},
-      {:addr_npi, {:integer, 1}},
-      {:address_range, {:c_octet_string, {:max, 41}}}
-    ]
-  end
+      bind_transmitter_resp: [
+        {:system_id, {:c_octet_string, {:max, 16}}}
+      ],
 
-  def spec_for(:bind_receiver_resp) do
-    [
-      {:system_id, {:c_octet_string, {:max, 16}}}
-    ]
-  end
+      bind_receiver: [
+        {:system_id, {:c_octet_string, {:max, 16}}},
+        {:password,  {:c_octet_string, {:max, 9}}},
+        {:system_type,  {:c_octet_string, {:max, 13}}},
+        {:interface_version, {:integer, 1}},
+        {:addr_ton, {:integer, 1}},
+        {:addr_npi, {:integer, 1}},
+        {:address_range, {:c_octet_string, {:max, 41}}}
+      ],
 
-  def spec_for(:bind_transceiver) do
-    [
-      {:system_id, {:c_octet_string, {:max, 16}}},
-      {:password,  {:c_octet_string, {:max, 9}}},
-      {:system_type,  {:c_octet_string, {:max, 13}}},
-      {:interface_version, {:integer, 1}},
-      {:addr_ton, {:integer, 1}},
-      {:addr_npi, {:integer, 1}},
-      {:address_range, {:c_octet_string, {:max, 41}}}
-    ]
-  end
+      bind_receiver_resp: [
+        {:system_id, {:c_octet_string, {:max, 16}}}
+      ],
 
-  def spec_for(:bind_transceiver_resp) do
-    [
-      {:system_id, {:c_octet_string, {:max, 16}}}
-    ]
-  end
+      bind_transceiver: [
+        {:system_id, {:c_octet_string, {:max, 16}}},
+        {:password,  {:c_octet_string, {:max, 9}}},
+        {:system_type,  {:c_octet_string, {:max, 13}}},
+        {:interface_version, {:integer, 1}},
+        {:addr_ton, {:integer, 1}},
+        {:addr_npi, {:integer, 1}},
+        {:address_range, {:c_octet_string, {:max, 41}}}
+      ],
 
-  def spec_for(:outbind) do
-    [
-      {:system_id, {:c_octet_string, {:max, 16}}},
-      {:password,  {:c_octet_string, {:max, 9}}}
-    ]
-  end
+      bind_transceiver_resp: [
+        {:system_id, {:c_octet_string, {:max, 16}}}
+      ],
 
-  def spec_for(:unbind) do
-    []
-  end
+      outbind: [
+        {:system_id, {:c_octet_string, {:max, 16}}},
+        {:password,  {:c_octet_string, {:max, 9}}}
+      ],
 
-  def spec_for(:unbind_resp) do
-    []
-  end
+      unbind: [],
 
-  def spec_for(:generic_nack) do
-    []
-  end
+      unbind_resp: [],
 
-  def spec_for(:submit_sm) do
-    [
-      {:service_type, {:c_octet_string, {:max, 6}}},
-      {:source_addr_ton, {:integer, 1}},
-      {:source_addr_npi, {:integer, 1}},
-      {:source_addr, {:c_octet_string, {:max, 21}}},
-      {:dest_addr_ton, {:integer, 1}},
-      {:dest_addr_npi, {:integer, 1}},
-      {:destination_addr, {:c_octet_string, {:max, 21}}},
-      {:esm_class, {:integer, 1}},
-      {:protocol_id, {:integer, 1}},
-      {:priority_flag, {:integer, 1}},
-      {:schedule_delivery_time, {:c_octet_string, {:fixed, 17}}},
-      {:validity_period, {:c_octet_string, {:fixed, 17}}},
-      {:registered_delivery, {:integer, 1}},
-      {:replace_if_present_flag, {:integer, 1}},
-      {:data_coding, {:integer, 1}},
-      {:sm_default_msg_id, {:integer, 1}},
-      {:sm_length, {:integer, 1}},
-      {:short_message, {:octet_string, :sm_length}}
-    ]
-  end
+      generic_nack: [],
 
-  def spec_for(:submit_sm_resp) do
-    [
-      {:message_id, {:c_octet_string, {:max, 65}}}
-    ]
-  end
-
-  def spec_for(:submit_multi) do
-    [
-      {:service_type, {:c_octet_string, {:max, 6}}},
-      {:source_addr_ton, {:integer, 1}},
-      {:source_addr_npi, {:integer, 1}},
-      {:source_addr, {:c_octet_string, {:max, 21}}},
-      {:number_of_dests, {:integer, 1}},
-      {:dest_addresses, {:times, :number_of_dests, [
-        {:dest_flag, {:integer, 1}},
-        {:case,
-          [{:dest_flag, 1, [
-            {:dest_addr_ton, {:integer, 1}},
-            {:dest_addr_npi, {:integer, 1}},
-            {:destination_addr, {:c_octet_string, {:max, 21}}}
-          ]},
-          {:dest_flag, 2, [
-            {:dl_name, {:c_octet_string, {:max, 21}}}
-          ]}]
-        }
-      ]}},
-      {:esm_class, {:integer, 1}},
-      {:protocol_id, {:integer, 1}},
-      {:priority_flag, {:integer, 1}},
-      {:schedule_delivery_time, {:c_octet_string, {:fixed, 17}}},
-      {:validity_period, {:c_octet_string, {:fixed, 17}}},
-      {:registered_delivery, {:integer, 1}},
-      {:replace_if_present_flag, {:integer, 1}},
-      {:data_coding, {:integer, 1}},
-      {:sm_default_msg_id, {:integer, 1}},
-      {:sm_length, {:integer, 1}},
-      {:short_message, {:octet_string, :sm_length}}
-    ]
-  end
-
-  def spec_for(:submit_multi_resp) do
-    [
-      {:message_id, {:c_octet_string, {:max, 65}}},
-      {:no_unsuccess, {:integer, 1}},
-      {:unsuccess_smes, {:times, :no_unsuccess}, [
+      submit_sm: [
+        {:service_type, {:c_octet_string, {:max, 6}}},
+        {:source_addr_ton, {:integer, 1}},
+        {:source_addr_npi, {:integer, 1}},
+        {:source_addr, {:c_octet_string, {:max, 21}}},
         {:dest_addr_ton, {:integer, 1}},
         {:dest_addr_npi, {:integer, 1}},
         {:destination_addr, {:c_octet_string, {:max, 21}}},
-        {:error_status_code, {:integer, 4}}
-      ]}
-    ]
-  end
+        {:esm_class, {:integer, 1}},
+        {:protocol_id, {:integer, 1}},
+        {:priority_flag, {:integer, 1}},
+        {:schedule_delivery_time, {:c_octet_string, {:fixed, 17}}},
+        {:validity_period, {:c_octet_string, {:fixed, 17}}},
+        {:registered_delivery, {:integer, 1}},
+        {:replace_if_present_flag, {:integer, 1}},
+        {:data_coding, {:integer, 1}},
+        {:sm_default_msg_id, {:integer, 1}},
+        {:sm_length, {:integer, 1}},
+        {:short_message, {:octet_string, :sm_length}}
+      ],
 
-  def spec_for(:deliver_sm) do
-    [
-      {:service_type, {:c_octet_string, {:max, 6}}},
-      {:source_addr_ton, {:integer, 1}},
-      {:source_addr_npi, {:integer, 1}},
-      {:source_addr, {:c_octet_string, {:max, 21}}},
-      {:dest_addr_ton, {:integer, 1}},
-      {:dest_addr_npi, {:integer, 1}},
-      {:destination_addr, {:c_octet_string, {:max, 21}}},
-      {:esm_class, {:integer, 1}},
-      {:protocol_id, {:integer, 1}},
-      {:priority_flag, {:integer, 1}},
-      {:schedule_delivery_time, {:c_octet_string, {:fixed, 17}}},
-      {:validity_period, {:c_octet_string, {:fixed, 17}}},
-      {:registered_delivery, {:integer, 1}},
-      {:replace_if_present_flag, {:integer, 1}},
-      {:data_coding, {:integer, 1}},
-      {:sm_default_msg_id, {:integer, 1}},
-      {:sm_length, {:integer, 1}},
-      {:short_message, {:octet_string, :sm_length}}
-    ]
-  end
+      submit_sm_resp: [
+        {:message_id, {:c_octet_string, {:max, 65}}}
+      ],
 
-  def spec_for(:deliver_sm_resp) do
-    [
-      {:message_id, {:c_octet_string, {:max, 65}}}
-    ]
-  end
+      submit_multi: [
+        {:service_type, {:c_octet_string, {:max, 6}}},
+        {:source_addr_ton, {:integer, 1}},
+        {:source_addr_npi, {:integer, 1}},
+        {:source_addr, {:c_octet_string, {:max, 21}}},
+        {:number_of_dests, {:integer, 1}},
+        {:dest_addresses, {:times, :number_of_dests, [
+          {:dest_flag, {:integer, 1}},
+          {:case,
+            [{:dest_flag, 1, [
+              {:dest_addr_ton, {:integer, 1}},
+              {:dest_addr_npi, {:integer, 1}},
+              {:destination_addr, {:c_octet_string, {:max, 21}}}
+            ]},
+            {:dest_flag, 2, [
+              {:dl_name, {:c_octet_string, {:max, 21}}}
+            ]}]
+          }
+        ]}},
+        {:esm_class, {:integer, 1}},
+        {:protocol_id, {:integer, 1}},
+        {:priority_flag, {:integer, 1}},
+        {:schedule_delivery_time, {:c_octet_string, {:fixed, 17}}},
+        {:validity_period, {:c_octet_string, {:fixed, 17}}},
+        {:registered_delivery, {:integer, 1}},
+        {:replace_if_present_flag, {:integer, 1}},
+        {:data_coding, {:integer, 1}},
+        {:sm_default_msg_id, {:integer, 1}},
+        {:sm_length, {:integer, 1}},
+        {:short_message, {:octet_string, :sm_length}}
+      ],
 
-  def spec_for(:data_sm) do
-    [
-      {:service_type, {:c_octet_string, {:max, 6}}},
-      {:source_addr_ton, {:integer, 1}},
-      {:source_addr_npi, {:integer, 1}},
-      {:source_addr, {:c_octet_string, {:max, 65}}},
-      {:dest_addr_ton, {:integer, 1}},
-      {:dest_addr_npi, {:integer, 1}},
-      {:destination_addr, {:c_octet_string, {:max, 65}}},
-      {:esm_class, {:integer, 1}},
-      {:data_coding, {:integer, 1}}
-    ]
-  end
+      submit_multi_resp: [
+        {:message_id, {:c_octet_string, {:max, 65}}},
+        {:no_unsuccess, {:integer, 1}},
+        {:unsuccess_smes, {:times, :no_unsuccess}, [
+          {:dest_addr_ton, {:integer, 1}},
+          {:dest_addr_npi, {:integer, 1}},
+          {:destination_addr, {:c_octet_string, {:max, 21}}},
+          {:error_status_code, {:integer, 4}}
+        ]}
+      ],
 
-  def spec_for(:data_sm_resp) do
-    [
-      {:message_id, {:c_octet_string, {:max, 65}}}
-    ]
-  end
+      deliver_sm: [
+        {:service_type, {:c_octet_string, {:max, 6}}},
+        {:source_addr_ton, {:integer, 1}},
+        {:source_addr_npi, {:integer, 1}},
+        {:source_addr, {:c_octet_string, {:max, 21}}},
+        {:dest_addr_ton, {:integer, 1}},
+        {:dest_addr_npi, {:integer, 1}},
+        {:destination_addr, {:c_octet_string, {:max, 21}}},
+        {:esm_class, {:integer, 1}},
+        {:protocol_id, {:integer, 1}},
+        {:priority_flag, {:integer, 1}},
+        {:schedule_delivery_time, {:c_octet_string, {:fixed, 17}}},
+        {:validity_period, {:c_octet_string, {:fixed, 17}}},
+        {:registered_delivery, {:integer, 1}},
+        {:replace_if_present_flag, {:integer, 1}},
+        {:data_coding, {:integer, 1}},
+        {:sm_default_msg_id, {:integer, 1}},
+        {:sm_length, {:integer, 1}},
+        {:short_message, {:octet_string, :sm_length}}
+      ],
 
-  def spec_for(:query_sm) do
-    [
-      {:message_id, {:c_octet_string, {:max, 65}}},
-      {:source_addr_ton, {:integer, 1}},
-      {:source_addr_npi, {:integer, 1}},
-      {:source_addr, {:c_octet_string, {:max, 21}}}
-    ]
-  end
+      deliver_sm_resp: [
+        {:message_id, {:c_octet_string, {:max, 65}}}
+      ],
 
-  def spec_for(:query_sm_resp) do
-    [
-      {:message_id, {:c_octet_string, {:max, 65}}},
-      {:final_date, {:c_octet_string, {:max, 17}}},
-      {:message_state, {:integer, 1}},
-      {:error_code, {:integer, 1}}
-    ]
-  end
+      data_sm: [
+        {:service_type, {:c_octet_string, {:max, 6}}},
+        {:source_addr_ton, {:integer, 1}},
+        {:source_addr_npi, {:integer, 1}},
+        {:source_addr, {:c_octet_string, {:max, 65}}},
+        {:dest_addr_ton, {:integer, 1}},
+        {:dest_addr_npi, {:integer, 1}},
+        {:destination_addr, {:c_octet_string, {:max, 65}}},
+        {:esm_class, {:integer, 1}},
+        {:data_coding, {:integer, 1}}
+      ],
 
-  def spec_for(:cancel_sm) do
-    [
-      {:service_type, {:c_octet_string, {:max, 6}}},
-      {:message_id, {:c_octet_string, {:max, 65}}},
-      {:source_addr_ton, {:integer, 1}},
-      {:source_addr_npi, {:integer, 1}},
-      {:source_addr, {:c_octet_string, {:max, 21}}},
-      {:dest_addr_ton, {:integer, 1}},
-      {:dest_addr_npi, {:integer, 1}},
-      {:destination_addr, {:c_octet_string, {:max, 21}}}
-    ]
-  end
+      data_sm_resp: [
+        {:message_id, {:c_octet_string, {:max, 65}}}
+      ],
 
-  def spec_for(:cancel_sm_resp) do
-    []
-  end
+      query_sm: [
+        {:message_id, {:c_octet_string, {:max, 65}}},
+        {:source_addr_ton, {:integer, 1}},
+        {:source_addr_npi, {:integer, 1}},
+        {:source_addr, {:c_octet_string, {:max, 21}}}
+      ],
 
-  def spec_for(:replace_sm) do
-    [
-      {:message_id, {:c_octet_string, {:max, 65}}},
-      {:source_addr_ton, {:integer, 1}},
-      {:source_addr_npi, {:integer, 1}},
-      {:source_addr, {:c_octet_string, {:max, 21}}},
-      {:schedule_delivery_time, {:c_octet_string, {:fixed, 17}}},
-      {:validity_period, {:c_octet_string, {:fixed, 17}}},
-      {:registered_delivery, {:integer, 1}},
-      {:sm_default_msg_id, {:integer, 1}},
-      {:sm_length, {:integer, 1}},
-      {:short_message, {:octet_string, :sm_length}}
-    ]
-  end
+      query_sm_resp: [
+        {:message_id, {:c_octet_string, {:max, 65}}},
+        {:final_date, {:c_octet_string, {:max, 17}}},
+        {:message_state, {:integer, 1}},
+        {:error_code, {:integer, 1}}
+      ],
 
-  def spec_for(:replace_sm_resp) do
-    []
-  end
+      cancel_sm: [
+        {:service_type, {:c_octet_string, {:max, 6}}},
+        {:message_id, {:c_octet_string, {:max, 65}}},
+        {:source_addr_ton, {:integer, 1}},
+        {:source_addr_npi, {:integer, 1}},
+        {:source_addr, {:c_octet_string, {:max, 21}}},
+        {:dest_addr_ton, {:integer, 1}},
+        {:dest_addr_npi, {:integer, 1}},
+        {:destination_addr, {:c_octet_string, {:max, 21}}}
+      ],
 
-  def spec_for(:enquire_link) do
-    []
-  end
+      cancel_sm_resp: [],
 
-  def spec_for(:enquire_link_resp) do
-    []
-  end
+      replace_sm: [
+        {:message_id, {:c_octet_string, {:max, 65}}},
+        {:source_addr_ton, {:integer, 1}},
+        {:source_addr_npi, {:integer, 1}},
+        {:source_addr, {:c_octet_string, {:max, 21}}},
+        {:schedule_delivery_time, {:c_octet_string, {:fixed, 17}}},
+        {:validity_period, {:c_octet_string, {:fixed, 17}}},
+        {:registered_delivery, {:integer, 1}},
+        {:sm_default_msg_id, {:integer, 1}},
+        {:sm_length, {:integer, 1}},
+        {:short_message, {:octet_string, :sm_length}}
+      ],
 
-  def spec_for(:alert_notification) do
-    [
-      {:source_addr_ton, {:integer, 1}},
-      {:source_addr_npi, {:integer, 1}},
-      {:source_addr, {:c_octet_string, {:max, 61}}},
-      {:esme_addr_ton, {:integer, 1}},
-      {:esme_addr_npi, {:integer, 1}},
-      {:esme_addr, {:c_octet_string, {:max, 61}}}
-    ]
-  end
+      replace_sm_resp: [],
 
-  def spec_for(_) do
-    []
+      enquire_link: [],
+
+      enquire_link_resp: [],
+
+      alert_notification: [
+        {:source_addr_ton, {:integer, 1}},
+        {:source_addr_npi, {:integer, 1}},
+        {:source_addr, {:c_octet_string, {:max, 61}}},
+        {:esme_addr_ton, {:integer, 1}},
+        {:esme_addr_npi, {:integer, 1}},
+        {:esme_addr, {:c_octet_string, {:max, 61}}}
+      ]
+    }
   end
+  
 end
