c90cbd57989e5b8027812768b10a5181230ea2b8
Ilya Averyanov <av@fun-box.ru>
2018-05-23 13:14:23 +0300

    Updated version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Smppex.Mixfile do
   def project do
     [
       app: :smppex,
-      version: "2.2.4",
+      version: "2.2.5",
       elixir: "~> 1.1",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
